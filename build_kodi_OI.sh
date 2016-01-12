a20c1bc50bed0d95122ad394afcef89aded553ba
phorcys <phorcys@126.com>
2016-01-13 04:51:01 +0800

    fix build script

--- build_kodi_OI.sh
+++ build_kodi_OI.sh
@@ -16,14 +16,14 @@ patch -p1 < ../patch/007-third-party-library.patch
 patch -p1 < ../patch/008-libupnp-temp-fix.patch
 patch -p1 < ../patch/009-fix-solaris-FILE-no__file.patch
 patch -p1 < ../patch/099-need-fix-disabled.patch
-
+patch -p1 < ../patch/010-fix-charset-iconv.patch
 else
 
 	cd xbmc
 fi
 
 CC=gcc CXX=g++ bash bootstrap
-CC=gcc CXX=g++ LD=/usr/gnu/bin/ld PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH PATH=/usr/mariadb/5.5/bin:$PATH bash configure --prefix=~/kodi/build/ --disable-ssh LDFLAGS="-L/usr/lib -L/usr/local/lib -L/usr/sfw/lib"
+CC=gcc CXX=g++ LD=/usr/gnu/bin/ld PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH PATH=/usr/mariadb/5.5/bin:$PATH bash configure --prefix=${HOME}/kodi/build/ --disable-ssh LDFLAGS="-L/usr/lib -L/usr/local/lib -L/usr/sfw/lib"
 CC=gcc CXX=g++ gmake -j4
 CC=gcc CXX=g++ gmake install
 
