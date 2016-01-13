85ac2c846ad34b8d72cd0e1b00fc912033ce6ada
phorcys <phorcys@126.com>
2016-01-13 11:40:02 +0800

    fix network, fix audio device detection

--- run_kodi_OI.sh
+++ run_kodi_OI.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib:/usr/sfw/lib KODI_HOME=~/kodi/build/share/kodi KODI_BIN_HOME=~/kodi/build/lib/kodi LIBGL_DEBUG=verbose ~/kodi/build/lib/kodi/kodi.bin --debug
+LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib:/usr/sfw/lib KODI_HOME=~/kodi/build/share/kodi KODI_BIN_HOME=~/kodi/build/lib/kodi LIBGL_DEBUG=verbose ~/kodi/build/lib/kodi/kodi.bin --debug
